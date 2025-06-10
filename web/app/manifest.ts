91152fa0f3d82beb383a44f20676a655771baa92
nwleedev <nw.lee@outlook.com>
2025-06-11 00:28:59 +0900

    feat: metadata

---
+++ web/app/manifest.ts
@@ -0,0 +1,135 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Use Textify",
+    short_name: "Textify",
+    description: "Share your awesome prompts with users",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+
+    icons: [
+      {
+        src: "/static/android-icon-36x36.png",
+        sizes: "36x36",
+        type: "image/png",
+      },
+      {
+        src: "/static/android-icon-48x48.png",
+        sizes: "48x48",
+        type: "image/png",
+      },
+      {
+        src: "/static/android-icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        src: "/static/android-icon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        src: "/static/android-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        src: "/static/android-icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-57x57.png",
+        sizes: "57x57",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-60x60.png",
+        sizes: "60x60",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-76x76.png",
+        sizes: "76x76",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-114x114.png",
+        sizes: "114x114",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-120x120.png",
+        sizes: "120x120",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-180x180.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon-precomposed.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/static/apple-icon.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/static/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/static/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        src: "/static/favicon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+      },
+      {
+        src: "/static/ms-icon-70x70.png",
+        sizes: "70x70",
+        type: "image/png",
+      },
+      {
+        src: "/static/ms-icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+      },
+      {
+        src: "/static/ms-icon-150x150.png",
+        sizes: "150x150",
+        type: "image/png",
+      },
+      {
+        src: "/static/ms-icon-310x310.png",
+        sizes: "310x310",
+        type: "image/png",
+      },
+    ],
+  };
+}
---
+++ web/app/sw.ts
@@ -0,0 +1,25 @@
+import { defaultCache } from "@serwist/next/worker";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
+
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+  }
+}
+
+declare const self: ServiceWorkerGlobalScope;
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
+
+self.addEventListener("install", serwist.handleInstall);
+self.addEventListener("activate", serwist.handleActivate);
+self.addEventListener("message", serwist.handleCache);
+
+serwist.addEventListeners();
