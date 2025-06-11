f7e69fb9fc77e0bcd48494b9537ec3dccdae0e42
nwleedev <nw.lee@outlook.com>
2025-06-11 23:22:29 +0900

    fix: handle error

--- web/app/(entry)/(feeds)/feeds/[id]/page.tsx
+++ web/app/(entry)/(feeds)/feeds/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { verifyUser } from "@/entities/auth/lib/verify";
 import { getFeedById } from "@/entities/feed/lib/api";
+import { FeedGridItem } from "@/entities/feed/lib/model";
 import { getFeedByIdQueryKey } from "@/entities/feed/lib/query-key";
 import UserProvider from "@/features/auth/lib/user/provider";
 import { getFeedByIdQuery } from "@/features/feed/lib/query";
@@ -7,6 +8,7 @@ import { createClient } from "@/shared/lib/pocketbase/server/client";
 import HydrationQuery from "@/shared/ui/hydration-query";
 import FeedDetail from "@/widgets/feeds/ui/detail";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 import { cache } from "react";
 
 const getFeedMetadata = cache(getFeedById);
@@ -18,7 +20,16 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const client = createClient();
   const { id } = params;
-  const feed = await getFeedMetadata(client, id);
+  let feed: FeedGridItem | null = null;
+  try {
+    feed = await getFeedMetadata(client, id);
+  } catch (error) {
+    // TODO: handle error
+    notFound();
+  }
+  if (!feed) {
+    notFound();
+  }
   const category = feed.expand.category.name;
   const tags = feed.expand.tags?.map((tag) => tag.name) ?? [];
   const keywords = [category, ...tags];
