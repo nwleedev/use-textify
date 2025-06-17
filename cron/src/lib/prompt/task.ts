eab37d7f92f0571bde05ce36922af35e5538b1f5
nwleedev <nw.lee@outlook.com>
2025-06-17 16:12:41 +0900

    fix: prompt with variables

--- cron/src/lib/prompt/task.ts
+++ cron/src/lib/prompt/task.ts
@@ -11,7 +11,9 @@ export function createFeedPrompt(concept: string) {
   Please write the main prompt consisting of over 80 words as much as possible.
   For the short description, if it is like "This prompt gives ...", please write it except for the part of "This prompt gives".
   The list of tags at the prompt is a list of words. Each tag should have the length of 16 characters or less.
-  Variables are the variables required for prompts. Each variable item must consist of a variable and a variable description. A variable description is a single sentence consisting of 14-16 words about what a variable does. If a variable is written in a prompt, it must also be specified in a variable item. For example, if the prompt specifies ... {{text}} ... or ... {{title}} ..., the variable list must have a text, or title item.
+  Variables are the variables required for prompts. Each variable item must consist of a variable and a variable description. A variable description is a single sentence consisting of 14-16 words about what a variable does. 
+  The main prompt should use variables listed in variable list.
+  For example, If there are variables "text" or "title", it means that prompt have to use these variables like "... {{text}} ..." or "... {{title}} ...".
   Notices are not an additional input to the prompt. Notices are used to specify what you should be aware of before using the data that you prompt. For example, a notice would be like "LLM can make mistakes.". Do not include "LLM can make mistakes." directly.
   Each notice is a single sentence consisting of 10-12 words.
   Please answer in English and JSON format.
